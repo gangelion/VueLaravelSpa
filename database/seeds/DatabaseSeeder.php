6a9bb2d3f5224be502b9148acc865e0ccb6f5cba
gangelion <gangelion8@gmail.com>
2020-05-07 13:30:14 +0900

    タスク一覧取得API実装

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TaskTableSeeder::class);
+        $this->call(TasksTableSeeder::class);
     }
 }
