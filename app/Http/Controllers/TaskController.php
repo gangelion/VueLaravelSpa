1646e225d668450479dcad4232e4b295ce5556a7
gangelion <gangelion8@gmail.com>
2020-05-07 13:42:36 +0900

    タスク更新API実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -20,4 +20,11 @@ class TaskController extends Controller
     {
         return Task::create($request->all());
     }
+
+    public function update(Request $request, Task $task)
+    {
+        $task->update($request->all();
+
+        return $task;
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,5 @@ Route::get('/tasks', 'TaskController@index');
 Route::get('/tasks/{task}', 'TaskController@show');
 
 Route::post('/tasks', 'TaskController@store');
+
+Route::put('/tasks/{task}', 'TaskController@update');
