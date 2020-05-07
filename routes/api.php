53713b615d5ec53502e679dac114b249beaa175e
gangelion <gangelion8@gmail.com>
2020-05-07 13:34:43 +0900

    タスク詳細取得API実装

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/tasks', 'TaskController@index');
+
+Route::get('/tasks/{task}', 'TaskController@show');
