b2a3af3d1bb95ea3d953fe89d4b6180a7904fc8a
gangelion <gangelion8@gmail.com>
2020-05-11 22:18:35 +0900

    タスク編集view

--- resources/js/components/TaskEditComponent.vue
+++ resources/js/components/TaskEditComponent.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-sm-6">
-        <form>
+        <form v-on:submit.prevent="submit">
           <div class="form-group row">
             <label for="id" class="col-sm-3 col-form-label">ID</label>
             <input
@@ -10,20 +10,20 @@
               class="col-sm-9 form-control-plaintext"
               readonly
               id="id"
-              v-bind:value="taskId"
+              v-model="task.id"
             />
           </div>
           <div class="form-group row">
             <label for="title" class="col-sm-3 col-form-label">Title</label>
-            <input type="text" class="col-sm-9 form-control" id="title" />
+            <input type="text" class="col-sm-9 form-control" id="title" v-model="task.title" />
           </div>
           <div class="form-group row">
             <label for="content" class="col-sm-3 col-form-label">Content</label>
-            <input type="text" class="col-sm-9 form-control" id="content" />
+            <input type="text" class="col-sm-9 form-control" id="content" v-model="task.content"/>
           </div>
           <div class="form-group row">
             <label for="person-in-charge" class="col-sm-3 col-form-label">Person In Charge</label>
-            <input type="text" class="col-sm-9 form-control" id="person-in-charge" />
+            <input type="text" class="col-sm-9 form-control" id="person-in-charge" v-model="task.person_in_charge"/>
           </div>
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
